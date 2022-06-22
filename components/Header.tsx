ed9eb1ef1ee530942cf67bd40b62b3dedf3318e5
Ridwan <kadriridwan0@gmail.com>
2022-06-22 15:17:57 +0000

    desktop layout

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 function Header() {
   return (
-    <div className="mx-4">
+    <div className="max-w-5xl mx-auto">
         <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
             <div className="container flex flex-wrap justify-between items-center mx-auto">
                 <Link href="/" className="flex items-center">
